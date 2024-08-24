1eb61189177e4d0ad489d76819ec4bf9da0161d7
GOKUL-A-N <gokulcricketer141103@gmail.com>
2024-08-24 23:18:19 +0530

    Fix : modified cors policy

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ dotenv.config();
 
 app.use(express.json());
 
-app.use(cors({
-    origin: '*'
-}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 app.use(cookieParser());
 
